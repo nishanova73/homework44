a8cc4f581e32ca41a80993bc8f722e1179642ace
nishanova73 <madifelice77@gmail.com>
2021-12-20 18:02:05 +0600

    Added functionality to produce necessary numbers to play the game

--- lab/webapp/views.py
+++ lab/webapp/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
 
+import random
+
+def getDigits(num):
+    return [int(i) for i in str(num)]
+
+def noDuplicates(num):
+    num_li = getDigits(num)
+    if len(num_li) == len(set(num_li)):
+        return True
+    else:
+        return False
+
+def generateNum():
+    while True:
+        num = random.randint(1000,9999)
+        if noDuplicates(num):
+            return num
+
+FIRST_TIME = True
+COMPUTER = None
+HISTORY = []
+
 # Create your views here.
 def main_page_view(request):
     return render(request, 'main_page.html')
